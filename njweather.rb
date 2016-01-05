c3bccccb8f47729987b8ca5e231411d34a572b03
salmunoz <munoz.sal3@gmail.com>
2016-01-04 19:39:00 -0600

    Partially edited

--- njweather.rb
+++ njweather.rb
@@ -15,5 +15,8 @@ p weather_spread[0].class
 new_weather << weather_spread[0].split("\n")
 puts "___________________________"
 puts new_weather[2]
+
+
+
 #Now that I am able to open and read the actual file, I need to change the class. As it stands, it is currently a File class, when I need it to be a string class. Once I get it to be a string, I can take it and .split("\n" (what I think is the spliting by newline))
 
